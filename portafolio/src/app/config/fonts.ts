572473ef3dd705e107d9925139fc1e7fd7497481
Yoel <yoelmontelongoo0@gmail.com>
2025-09-01 12:03:06 -0300

    Update fonts.ts

--- portafolio/src/app/config/fonts.ts
+++ portafolio/src/app/config/fonts.ts
@@ -3,9 +3,11 @@ import { Outfit, RocknRoll_One} from "next/font/google";
 export const rockroll = RocknRoll_One({
     variable:"--font-exo2-title",
     weight:"400",
+    preload: false
 })
 
 export const outfit = Outfit({
     variable:"--font-outfit-text",
     weight:"400",
+    preload: false
 })
